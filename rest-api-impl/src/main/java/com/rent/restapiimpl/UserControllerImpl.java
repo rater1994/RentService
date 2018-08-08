33fbaa29a45ab2fe4cfc1c507840a3921d18fed7
rater1994 <athauf94@gmail.com>
2018-08-08 10:53:50 +0300

    some new updatE

--- rest-api-impl/src/main/java/com/rent/restapiimpl/UserControllerImpl.java
+++ rest-api-impl/src/main/java/com/rent/restapiimpl/UserControllerImpl.java
@@ -1,15 +1,27 @@
 package com.rent.restapiimpl;
 
 import com.rent.model.dto.UserDto;
+import com.rent.model.repository.UserRepository;
 import com.rent.restapi.User.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserControllerImpl implements UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public List<UserDto> getAllUsers() {
-        return null;
+        List<UserDto> list = new ArrayList<>();
+
+        userRepository.findAll().forEach(user -> {
+            list.add(user.toDto());
+        });
+        return list;
     }
 }
